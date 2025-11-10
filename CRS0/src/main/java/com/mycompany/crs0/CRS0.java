0554538499684aadd2576bee6e88e1e2ab7c6c92
M7MDFARES1 <mohamedfares9560@gmail.com>
2025-11-10 18:49:05 +0800

    Test

---
+++ CRS0/src/main/java/com/mycompany/crs0/CRS0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crs0;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CRS0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
