160316eb966c243f56c1d050b9b4a2eafee12b65
M7MDFARES1 <mohamedfares9560@gmail.com>
2025-11-18 22:26:35 +0800

    Test CRS1

---
+++ CRS1/src/main/java/com/mycompany/crs1/CRS1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crs1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CRS1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
