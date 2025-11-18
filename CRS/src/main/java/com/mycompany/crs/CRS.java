1f97212e6ef29bdeaa74c4e33efca942b3487962
konda <konda@Omars>
2025-11-18 21:57:33 +0800

    kokok

---
+++ CRS/src/main/java/com/mycompany/crs/CRS.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crs;
+
+/**
+ *
+ * @author konda
+ */
+public class CRS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+//ayre feek
